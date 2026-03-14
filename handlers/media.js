1a6f6a954d362f0a2596850b2b4a205bafd2fdda
nahidx07 <muhammadnahidhossen70@gmail.com>
2026-03-14 19:57:23 +0600

    Delete handlers directory

--- handlers/media.js
+++
@@ -1,9 +0,0 @@
-const db = require('../lib/firebase');
-module.exports = async (ctx) => {
-  const waitMsg = await ctx.reply("⚡ প্রসেসিং...");
-  const slug = `file${Math.random().toString(36).substring(2, 12)}`;
-  const sent = await ctx.telegram.copyMessage(process.env.CHANNEL_ID, ctx.chat.id, ctx.message.message_id, { caption: ctx.message.caption || "" });
-  await db.collection('videos').doc(slug).set({ slug, message_id: sent.message_id, uploader_id: ctx.from.id, created_at: new Date().toISOString() });
-  await ctx.telegram.deleteMessage(ctx.chat.id, waitMsg.message_id);
-  ctx.reply(`✅ সেভ হয়েছে! আইডি: <code>${slug}</code>`, { parse_mode: 'HTML' });
-};
